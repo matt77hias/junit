dc08e55ca6a0fa3cf7445276db26256c8904654a
Mattias Buelens <mattias@buelens.com>
2013-11-04 11:27:58 +0100

    Collect bytecodes of all methods in one go.
    Added source JAR for ASM.

--- src/main/java/kuleuven/group2/HelloWorld.java
+++ src/main/java/kuleuven/group2/HelloWorld.java
@@ -3,11 +3,10 @@ package kuleuven.group2;
 public class HelloWorld {
 
 	public void a() {
-		b();
+		b(5);
 	}
 
-	public void b() {
-		int foo= 5;
+	public void b(int foo) {
 		System.out.println("Hallo " + foo);
 	}
 
