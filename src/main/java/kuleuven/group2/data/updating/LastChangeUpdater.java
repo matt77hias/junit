a1042373d98cca1a0948148155dfa52b3cd2a348
vital-dhaveloose <vital.dhaveloose@hotmail.be>
2013-11-11 19:04:46 +0100

    added the updaters

---
+++ src/main/java/kuleuven/group2/data/updating/LastChangeUpdater.java
@@ -0,0 +1,13 @@
+package kuleuven.group2.data.updating;
+
+/**
+ * Updates the data when a method is changed.
+ * @author vital.dhaveloose
+ *
+ */
+public class LastChangeUpdater {
+	//TODO: gebruikt/implemnteerd? een storeListener
+	
+	//TODO: bewerkt de geteste methodes (TestedMethod)
+
+}
