b90d89372c8397a989d97aee4f5c6b3bcc89748f
vital-dhaveloose <vital.dhaveloose@hotmail.be>
2013-11-11 19:04:46 +0100

    added the updaters

---
+++ src/main/java/kuleuven/group2/data/updating/MethodTestLinkUpdater.java
@@ -0,0 +1,15 @@
+package kuleuven.group2.data.updating;
+
+/**
+ * Updates the link between methods and tests, when tests are run.
+ * @author vital.dhaveloose
+ *
+ */
+public class MethodTestLinkUpdater {
+	
+	//TODO: gebruikt/implementeerd? een CurrentTestHolder en de OSSRewriter
+	//TODO: zie ook de methodLinker en de MethodLinkRegistryMonitor
+	
+	//TODO: bewerkt de geteste methodes (TestedMethod)
+
+}
---
+++ src/main/java/kuleuven/group2/data/updating/TestResultUpdater.java
@@ -0,0 +1,13 @@
+package kuleuven.group2.data.updating;
+
+/**
+ * Updates the data with the latest results when tests are run.
+ * @author vital.dhaveloose
+ *
+ */
+public class TestResultUpdater {
+
+	//TODO: gebruikt een runlisener
+	//TODO: bewerkt de tests (Test)
+	
+}
