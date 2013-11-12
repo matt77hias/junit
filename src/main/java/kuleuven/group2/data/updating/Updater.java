1e4991ede6e62fc4589e9db6d42e87cd810dc45a
vital-dhaveloose <vital.dhaveloose@hotmail.be>
2013-11-12 10:19:08 +0100

    added abstract 'Updater'

---
+++ src/main/java/kuleuven/group2/data/updating/Updater.java
@@ -0,0 +1,14 @@
+package kuleuven.group2.data.updating;
+
+import kuleuven.group2.data.TestDatabase;
+
+/**
+ * A class that updates the information in a TestDatabase.
+ * @author Vital D'haveloose
+ *
+ */
+public abstract class Updater {
+
+	protected TestDatabase database;
+	
+}
