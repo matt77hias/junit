c7b2d78dde5b0b82a583e777635f50bfc07909f8
vital-dhaveloose <vital.dhaveloose@hotmail.be>
2013-11-15 14:53:13 +0100

    refined method change listener interface after speaking to @MattiasBuelens

--- src/main/java/kuleuven/group2/data/updating/MethodChangeListener.java
+++ src/main/java/kuleuven/group2/data/updating/MethodChangeListener.java
@@ -1,10 +1,9 @@
 package kuleuven.group2.data.updating;
 
-import java.lang.reflect.Method;
 import java.util.Date;
 
 public interface MethodChangeListener {
 
-	public void changed(Method method, Date time);
+	public void changed(String signature, Date time);
 	
 }
