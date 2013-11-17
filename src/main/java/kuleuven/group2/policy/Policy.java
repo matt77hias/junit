85e54d8b21c88b7d23f2149d84f4c3002bb2cc09
matt77hias <matthias.moulin@gmail.com>
2013-11-17 20:06:38 +0100

    Policy specified

--- src/main/java/kuleuven/group2/policy/Policy.java
+++ src/main/java/kuleuven/group2/policy/Policy.java
@@ -5,9 +5,29 @@ import java.util.List;
 
 import kuleuven.group2.data.Test;
 
+/**
+ * An interface that has to be implemented for the different policies
+ * according to the order in which tests need to be executed.
+ * 
+ * @author 	Group 2
+ * @version	17 November 2013
+ *
+ */
 public interface Policy extends Comparator<Test> {
 	
-	public Test[] getSortedTestAccordingToPolicy(Test[] tests);
-			
-	public List<Test> getSortedTestAccordingToPolicy(List<Test> tests);
+	/**
+	 * Sorts the given tests according to this policy.
+	 * 
+	 * @param 	tests
+	 * 			The tests that needs to be sorted.
+	 */
+	public void getSortedTestAccordingToPolicy(Test[] tests);
+		
+	/**
+	 * Sorts the given tests according to this policy.
+	 * 
+	 * @param 	tests
+	 * 			The tests that needs to be sorted.
+	 */
+	public void getSortedTestAccordingToPolicy(List<Test> tests);
 }
