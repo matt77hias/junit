28bb5f3d887594fc5e0e7c3ee8213633c52e806e
vital-dhaveloose <vital.dhaveloose@hotmail.be>
2013-11-21 20:59:03 +0100

    updated AllTests

--- src/test/java/kuleuven/group2/AllTests.java
+++ src/test/java/kuleuven/group2/AllTests.java
@@ -3,6 +3,8 @@ package kuleuven.group2;
 import kuleuven.group2.compile.EclipseCompilerTest;
 import kuleuven.group2.data.DatabaseTests;
 import kuleuven.group2.filewatch.FolderWatcherTest;
+import kuleuven.group2.policy.PolicyTest;
+import kuleuven.group2.runner.TestRunnerTest;
 import kuleuven.group2.store.DirectoryStoreTest;
 
 import org.junit.runner.RunWith;
@@ -10,7 +12,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({DatabaseTests.class, EclipseCompilerTest.class, FolderWatcherTest.class, DirectoryStoreTest.class})
+@SuiteClasses({
+	EclipseCompilerTest.class,
+	DatabaseTests.class,
+	FolderWatcherTest.class,
+	PolicyTest.class,
+	TestRunnerTest.class,
+	DirectoryStoreTest.class,})
 public class AllTests {
 	
 }
