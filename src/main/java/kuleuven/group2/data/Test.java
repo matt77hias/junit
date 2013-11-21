1b7ee70bebf1083d3f0cc7ad24c053c79256e13c
Mattias Buelens <mattias@buelens.com>
2013-11-21 23:05:23 +0100

    Sort test runs only when retrieved.
    Simplified getFailurePercentage and getLastFailureTime.

--- src/main/java/kuleuven/group2/data/Test.java
+++ src/main/java/kuleuven/group2/data/Test.java
@@ -1,16 +1,18 @@
 package kuleuven.group2.data;
 
-import java.util.Collection;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
+import com.google.common.collect.Iterables;
 
 /**
- * A class that represents a test method. It also keeps track of the running
- * of this method, that is the timings and results.
+ * A class that represents a test method. It also keeps track of the running of
+ * this method, that is the timings and results.
  * 
  * @author Ruben Pieters, Vital D'haveloose
  */
@@ -18,13 +20,13 @@ public class Test {
 
 	protected String testClassName;
 	protected String testMethodName;
-	// sorted, last failures are first, first failures are last
-	protected LinkedList<TestRun> testRuns = new LinkedList<TestRun>();
-	
+
+	protected boolean isTestRunsSorted = true;
+	protected List<TestRun> testRuns = new LinkedList<TestRun>();
+
 	public Test(String testClassName, String testMethodName) {
-		super();
-		this.testClassName= testClassName;
-		this.testMethodName= testMethodName;
+		this.testClassName = checkNotNull(testClassName);
+		this.testMethodName = checkNotNull(testMethodName);
 	}
 
 	public String getTestClassName() {
@@ -34,69 +36,63 @@ public class Test {
 	public String getTestMethodName() {
 		return testMethodName;
 	}
-	
+
 	public boolean equalName(String testClassName, String testMethodName) {
 		return getTestClassName().equals(testClassName) && getTestMethodName().equals(testMethodName);
 	}
-	
+
 	public void addTestRun(TestRun testRun) {
 		testRuns.add(testRun);
-	
-		// TODO: FASTER: make use of the fact that the list is sorted to insert at the right spot with binary search
-		Collections.sort(testRuns, new Comparator<TestRun>() {
-			@Override
-			public int compare(TestRun o1, TestRun o2) {
-				return - o1.getTimeStamp().compareTo(o2.getTimeStamp());
-			}
-		});
+		isTestRunsSorted = false;
 	}
-	
-	public Collection<TestRun> getTestRuns() {
+
+	public List<TestRun> getTestRuns() {
+		if (!isTestRunsSorted) {
+			// Sort: last failures first
+			Collections.sort(testRuns, new Comparator<TestRun>() {
+				@Override
+				public int compare(TestRun o1, TestRun o2) {
+					return -o1.getTimeStamp().compareTo(o2.getTimeStamp());
+				}
+			});
+			isTestRunsSorted = true;
+		}
 		return Collections.unmodifiableList(testRuns);
 	}
-	
+
 	public float getFailurePercentage(int depth) {
-		float failed = 0;
-		float succeeded = 0;
-		
-		if (depth < 0)
-			depth = 0;
-		
-		int count = 0;
-		Iterator<TestRun> iterator = testRuns.iterator();
-		while(iterator.hasNext() && count < depth) {
-			TestRun testRun = iterator.next();
-			if (testRun.isFailedRun())
-				failed++;
-			if (testRun.isSuccessfulRun())
-				succeeded++;
-			count++;
+		int failed = 0;
+		int succeeded = 0;
+
+		if (depth < 0) depth = 0;
+
+		for (TestRun testRun : Iterables.limit(getTestRuns(), depth)) {
+			if (testRun.isFailedRun()) failed++;
+			if (testRun.isSuccessfulRun()) succeeded++;
 		}
-		
-		if (failed + succeeded == 0)
-			return 0;
-		
-		return failed / (failed + succeeded);
+
+		if (failed + succeeded == 0) return 0f;
+
+		return ((float) failed) / ((float) (failed + succeeded));
 	}
-	
+
 	public Date getLastFailureTime() {
-		if (testRuns.isEmpty())
-			return new Date(0);
-		
+		List<TestRun> testRuns = getTestRuns();
+		if (testRuns.isEmpty()) return new Date(0);
+
 		for (TestRun testRun : testRuns) {
-			if (testRun.isFailedRun())
-				return testRun.getTimeStamp();
+			if (testRun.isFailedRun()) return testRun.getTimeStamp();
 		}
-		
+
 		return new Date(0);
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((testClassName == null) ? 0 : testClassName.hashCode());
-		result = prime * result + ((testMethodName == null) ? 0 : testMethodName.hashCode());
+		result = prime * result + testClassName.hashCode();
+		result = prime * result + testMethodName.hashCode();
 		return result;
 	}
 
@@ -106,19 +102,15 @@ public class Test {
 		if (obj == null) return false;
 		if (getClass() != obj.getClass()) return false;
 		Test other = (Test) obj;
-		if (testClassName == null) {
-			if (other.testClassName != null) return false;
-		} else if (!testClassName.equals(other.testClassName)) return false;
-		if (testMethodName == null) {
-			if (other.testMethodName != null) return false;
-		} else if (!testMethodName.equals(other.testMethodName)) return false;
+		if (!testClassName.equals(other.testClassName)) return false;
+		if (!testMethodName.equals(other.testMethodName)) return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Test [testClassName=" + testClassName + ", testMethodName=" + testMethodName + ", testRuns=" + testRuns
-				+ "]";
+		return "Test [testClassName=" + testClassName + ", testMethodName=" + testMethodName + ", testRuns="
+				+ getTestRuns() + "]";
 	}
-	
+
 }
