1ae80b97b69ce7ada4f9bedb8f3b8742ff79db2a
vital-dhaveloose <vital.dhaveloose@hotmail.be>
2013-11-22 14:03:13 +0100

    let the testrunner inform the test runner listeners of the specified events

--- src/main/java/kuleuven/group2/testrunner/TestRunnerListener.java
+++ src/main/java/kuleuven/group2/testrunner/TestRunnerListener.java
@@ -6,19 +6,19 @@ import org.junit.runner.Result;
 
 /**
  * Interface for classes that have to listen to the starting and stopping
- * of individual tests as well as whole pipeline-runs.
+ * of individual tests as well as whole runs in the testing phase of the pipeline.
  * 
  * @author Group 2
  * @version 22 November 2013
  */
 public interface TestRunnerListener {
 	
-	public void pipelineRunStarted(Test[] tests);
+	public void pipelineTestRunStarted(Test[] tests);
 	
 	public void testStarted(Test test);
 	
 	public void testStopped(Result result);
 	
-	public void pipelineRunStopped(Result[] results);
+	public void pipelineTestRunStopped(Result[] results);
 
 }
