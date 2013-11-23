35e622ebdda6f3d5b006890ef3ea086a100f467d
Mattias Buelens <mattias@buelens.com>
2013-11-23 15:18:25 +0100

    Working on directory choosing.

---
+++ src/main/java/kuleuven/group2/ui/Main.java
@@ -0,0 +1,25 @@
+package kuleuven.group2.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane root = (Pane) FXMLLoader.load(getClass().getResource("Configuration.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
