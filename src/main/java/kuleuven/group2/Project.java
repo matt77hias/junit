7e2e80d6692fe7201817017536572c9200b95457
matt77hias <matthias.moulin@gmail.com>
2013-12-19 13:54:15 +0100

    Docu added

--- src/main/java/kuleuven/group2/Project.java
+++ src/main/java/kuleuven/group2/Project.java
@@ -11,17 +11,64 @@ import kuleuven.group2.store.StoreFilter;
 import kuleuven.group2.store.StoreWatcher;
 import kuleuven.group2.util.Consumer;
 
+/**
+ * A class of projects. A project contains three stores:
+ * one for the class (non-testing) source code, one for
+ * the test source code and one for all the binaries.
+ * 
+ * A project contains support for compiling and for
+ * letting consumers listen to them indirectly so that
+ * they can consume store events.
+ * 
+ * @author	Group 2
+ * @version	19 December 2013
+ *
+ */
 public class Project {
 
+	/**
+	 * The class source store of this project.
+	 */
 	protected final Store classSourceStore;
+	
+	/**
+	 * The class test source store of this project.
+	 */
 	protected final Store testSourceStore;
+	
+	/**
+	 * The class binary store of this project.
+	 */
 	protected final Store binaryStore;
 
+	/**
+	 * The reloading class store loader of this project.
+	 */
 	protected final ReloadingStoreClassLoader testClassLoader;
 
+	/**
+	 * The store watcher of the class source store
+	 * of this project.
+	 */
 	protected final StoreWatcher classSourceWatcher;
+	
+	/**
+	 * The store watcher of the test source store
+	 * of this project.
+	 */
 	protected final StoreWatcher testSourceWatcher;
 
+	/**
+	 * Creates a new project with given class source store,
+	 * test source store and binary store.
+	 * 
+	 * @param 	classSourceStore
+	 * 			The class source store for this new project.
+	 * @param 	testSourceStore
+	 * 			The test source store for this new project.
+	 * @param 	binaryStore
+	 * 			The binary store for this new project.
+	 */
 	public Project(Store classSourceStore, Store testSourceStore, Store binaryStore) {
 		this.classSourceStore = checkNotNull(classSourceStore);
 		this.testSourceStore = checkNotNull(testSourceStore);
@@ -33,18 +80,40 @@ public class Project {
 		this.testSourceWatcher = new StoreWatcher(testSourceStore, StoreFilter.SOURCE);
 	}
 
+	/**
+	 * Returns the class source store of this project.
+	 * 
+	 * @return	The class source store of this project.
+	 */
 	public Store getClassSourceStore() {
 		return classSourceStore;
 	}
 
+	/**
+	 * Returns the test source store of this project.
+	 * 
+	 * @return	The test source store of this project.
+	 */
 	public Store getTestSourceStore() {
 		return testSourceStore;
 	}
 
+	/**
+	 * Returns the binary store of this project.
+	 * 
+	 * @return	The binary store of this project.
+	 */
 	public Store getBinaryStore() {
 		return binaryStore;
 	}
 
+	/**
+	 * Starts listening in order to let the given consumer consume
+	 * store events.
+	 * 
+	 * @param 	consumer
+	 * 			The consumer.
+	 */
 	public void startListening(Consumer<StoreEvent> consumer) {
 		this.classSourceWatcher.registerConsumer(consumer);
 		this.testSourceWatcher.registerConsumer(consumer);
@@ -54,6 +123,13 @@ public class Project {
 		this.testSourceStore.startListening();
 	}
 
+	/**
+	 * Stops listening in order to let the given consumer consume
+	 * store events.
+	 * 
+	 * @param 	consumer
+	 * 			The consumer.
+	 */
 	public void stopListening(Consumer<StoreEvent> consumer) {
 		this.classSourceWatcher.unregisterConsumer(consumer);
 		this.testSourceWatcher.unregisterConsumer(consumer);
@@ -63,22 +139,58 @@ public class Project {
 		this.testSourceStore.stopListening();
 	}
 
+	/**
+	 * Returns the classloader of this project.
+	 * 
+	 * @return	The classloader of this project.
+	 */
 	public ClassLoader getClassLoader() {
 		return this.testClassLoader;
 	}
 
+	/**
+	 * Removes the wrapped instance of the classloader such that
+	 * the next time a ClassLoader method of the classloader of
+	 * this project is called, a new instance must be created.
+	 * This new instance won't have the loaded classes from the
+	 * previous instance, forcing it to load them again.
+	 */
 	public void reloadClasses() {
 		this.testClassLoader.reload();
 	}
 
+	/**
+	 * Checks if the given class name belongs to a class
+	 * stored in the binary store of this project.
+	 * 
+	 * @param 	className
+	 * 			The class name that needs be checked.
+	 * @return	True if and only if the given class name
+	 * 			belongs to a class stored in the binary
+	 * 			store of this project.
+	 */
 	public boolean isBinaryClass(String className) {
 		return getBinaryStore().contains(NameUtils.toBinaryName(className));
 	}
 
+	/**
+	 * Creates a compiler for the class source code
+	 * stored in the class source store of this project.
+	 * 
+	 * @return	A compiler for the class source code
+	 * 			stored in the class source store of this project.
+	 */
 	public JavaCompiler createClassCompiler() {
 		return new EclipseCompiler(getClassSourceStore(), getBinaryStore(), getClassLoader());
 	}
 
+	/**
+	 * Creates a compiler for the test source code
+	 * stored in the class source store of this project.
+	 * 
+	 * @return	A compiler for the test source code
+	 * 			stored in the class source store of this project.
+	 */
 	public JavaCompiler createTestCompiler() {
 		return new EclipseCompiler(getTestSourceStore(), getBinaryStore(), getClassLoader());
 	}
